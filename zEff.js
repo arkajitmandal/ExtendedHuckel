e88339fd9ae33e8391cc3288427d8aa6280d1ffc
arkajitmandal <arkajitmandal@gmail.com>
2018-06-13 17:05:21 -0400

    a function to calculate value corresponding to n, l quantum number

--- zEff.js
+++ zEff.js
@@ -1,1 +1,13 @@
 // This is the file to calculate zeff 
+var toGroupValue= function(nl){
+    // Asigns a value corresponding to orbital
+    // take n and l as [n,l] input
+    s=1;
+    p=1;
+    d=2;
+    f=3;
+    n = nl[0];
+    l = nl[1];
+    
+    return parseFloat(n)*10+eval(l);
+    }
