bf27a1dc93d22e5134ae91ccade2a13adea51a1c
arkajitmandal <arkajitmandal@gmail.com>
2018-06-15 09:39:41 -0400

    progress bar

--- atom.js
+++ atom.js
@@ -628,6 +628,6 @@ var eStructure= [
         ["Co",["1s2","2s2","2p6","3s2","3p6","3d6","4s2"]]
     ];
 
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-      }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+    }
