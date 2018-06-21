092108989aa28007aa8183f92baf2568b2a4a16d
arkajitmandal <arkajitmandal@gmail.com>
2018-06-21 18:48:59 -0400

    LUMO and SOMO function added

--- occupation.js
+++ occupation.js
@@ -4,6 +4,35 @@ function occupy(mol,pE = 0.1){
     let econfig = new Array(E.length);
     while (totalElectron>0){
         //find the lowest
-        
+        let L =  getLUMO(econfig);
+        let S = getSOMO(econfig);
+        if (L!=false){
+
+        }
+        else if (S !=false){
+            
+        }
+        else {
+            return false;
+        }
     }
+}
+//get LUMO
+function getLUMO(econfig){
+    for (var i=0;i<econfig.length;i++){
+        if (econfig[i]==0){
+            return i;
+        }
+    }
+    return false;
+}
+
+//get SOMO
+function getSOMO(econfig){
+    for (var i=0;i<econfig.length;i++){
+        if (econfig[i]==1){
+            return i;
+        }
+    }
+    return false;
 }
