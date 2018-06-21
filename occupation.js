b557ad414401b6a434c49a761c30f340a709ccf8
arkajitmandal <arkajitmandal@gmail.com>
2018-06-21 18:38:07 -0400

    occupation js initiated

---
+++ occupation.js
@@ -0,0 +1,9 @@
+function occupy(mol,pE = 0.1){
+    let totalElectron = mol.totalElectron;
+    let E = mol.Eig;
+    let econfig = new Array(E.length);
+    while (totalElectron>0){
+        //find the lowest
+        
+    }
+}
