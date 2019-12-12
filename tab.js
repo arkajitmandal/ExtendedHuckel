3c6919bcd3df04ee6997b761299503dfd17d6fcc
Arkajit Mandal <arkajitmandal@gmail.com>
2019-12-11 23:05:41 -0500

    file upload button Added

--- tab.js
+++ tab.js
@@ -1,6 +1,8 @@
 function thisTab(i){
     for(var k=0;k<4;k++){
-        document.getElementById("button"+String(k+1)).className = "inactiveButton"
+        document.getElementById("button"+String(k+1)).className = "inactiveButton";
+        document.getElementById("tab"+String(k+1)).style.display="none";
     }
     document.getElementById("button"+String(i)).className = "activeButton";
+    document.getElementById("tab"+String(i)).style.display="grid";
 }
