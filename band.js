8e7e420f20ad0156e7e33a635016b12ffc7ed20e
arkajitmandal <arkajitmandal@gmail.com>
2019-12-26 18:37:38 -0500

    computing moved xyz

--- band.js
+++ band.js
@@ -1,6 +1,16 @@
-// move molecules by n1 * v1 + n2 * v2 + n3 * v3 
+// get the molecule at n1 * v1 + n2 * v2 + n3 * v3 
 // nn = [n1,n2,n3] is integers that define the unit cell
 // vv = [v1,v2,v3] is lattice vectors 
-function moveMol(nn, vv) {
-
+function moveMol(atoms, nn, vv) {
+    let movedAtoms = new Array(atoms.length);
+    for (var iatom = 0; iatom < atoms.length; iatom++) {
+        for (var idim = 0; idim < nn.length; idim++) {
+            let vx = vv[0];
+            let vy = vv[1];
+            let vz = vv[2];
+            let x = nn[0] * vx + atoms[iatom].x;
+            let y = nn[0] * vy + atoms[iatom].y;
+            let z = nn[0] * vz + atoms[iatom].z;
+        }
+    }
 }
