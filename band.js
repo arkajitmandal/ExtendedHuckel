e57b2e5b889a77a33dc0f6a60e765608b0301d13
Arkajit Mandal <arkajitmandal@gmail.com>
2019-12-31 01:39:49 -0500

    modify movedAtoms function

--- band.js
+++ band.js
@@ -1,16 +1,21 @@
 // get the molecule at n1 * v1 + n2 * v2 + n3 * v3 (unit cell = n1, n2, n3)
 // nn = [n1,n2,n3] is integers that define the unit cell
-// vv = [v1,v2,v3] is lattice vectors v1 = [vx, vy, vz]
+// vv = [v1,v2,v3] is lattice vectors where v1 = [vx, vy, vz]
 function moveMol(atoms, nn, vv) {
     let movedAtoms = new Array(atoms.length);
     for (var iatom = 0; iatom < atoms.length; iatom++) {
         for (var idim = 0; idim < nn.length; idim++) {
-            let v1 = vv[0];
-            let v2 = vv[1];
-            let v3 = vv[2];
-            let x = nn[idim] * ( v1[0]  + atoms[iatom].x;
-            let y = nn[idim] * vy + atoms[iatom].y;
-            let z = nn[idim] * vz + atoms[iatom].z;
+            let x = atoms[iatom].x;
+            let y = atoms[iatom].y;
+            let z = atoms[iatom].z;
+            for (var iv = 0; iv < vv.length; iv++) {
+                let vx = vv[iv][0];
+                let vy = vv[iv][1];
+                let vz = vv[iv][2];
+                x += nn[idim] * vx;
+                y += nn[idim] * vy;
+                z += nn[idim] * vz;
+            }
             movedAtoms.push(new atom(atoms[iatom].S, x, y, z));
         }
     }
