abbdeda6c9031934f14fe5bbb105a54080aed993
Arkajit Mandal <arkajitmandal@gmail.com>
2019-12-31 01:22:31 -0500

    moveMol is wrong

--- band.js
+++ band.js
@@ -1,18 +1,32 @@
 // get the molecule at n1 * v1 + n2 * v2 + n3 * v3 (unit cell = n1, n2, n3)
 // nn = [n1,n2,n3] is integers that define the unit cell
-// vv = [v1,v2,v3] is lattice vectors 
+// vv = [v1,v2,v3] is lattice vectors v1 = [vx, vy, vz]
 function moveMol(atoms, nn, vv) {
     let movedAtoms = new Array(atoms.length);
     for (var iatom = 0; iatom < atoms.length; iatom++) {
         for (var idim = 0; idim < nn.length; idim++) {
-            let vx = vv[0];
-            let vy = vv[1];
-            let vz = vv[2];
-            let x = nn[0] * vx + atoms[iatom].x;
-            let y = nn[0] * vy + atoms[iatom].y;
-            let z = nn[0] * vz + atoms[iatom].z;
+            let v1 = vv[0];
+            let v2 = vv[1];
+            let v3 = vv[2];
+            let x = nn[idim] * ( v1[0]  + atoms[iatom].x;
+            let y = nn[idim] * vy + atoms[iatom].y;
+            let z = nn[idim] * vz + atoms[iatom].z;
             movedAtoms.push(new atom(atoms[iatom].S, x, y, z));
         }
     }
     return movedAtoms;
+}
+
+// Sij(k) = Σ exp(i k Rn) S(r-Rn)
+
+function bandSij( k, mol, vv, n = 2) {
+    let dim = vv.length ;
+    // 1D case 
+    if (dim == 1) {
+        for (let nx = -n; nx <= n + 1; nx++ ) {
+            let nn = [nx, 0, 0];
+            pass
+
+        }
+    }
 }
